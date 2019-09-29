3d2b31d2d03e2d9a96b827818af383394f4baefe
M4cs <mabridgland@protonmail.com>
2019-09-29 12:40:24 -0400

    Add

--- app/api_search.py
+++ app/api_search.py
@@ -4,11 +4,21 @@ from app.utils.search import get_output
 def search_parser():
     parser = reqparse.RequestParser()
     parser.add_argument('q', help='Query Search', required=True)
+    parser.add_argument('n')
+    parser.add_argument('a')
+    parser.add_argument('l')
     return parser
 
 class SearchAPI(Resource):
     def get(self):
         parser = search_parser()
         args = parser.parse_args()
+        query = args['q']
+        if args['n']:
+            query += ' ' + '-n' + args['n']
+        if args['a']:
+            query += ' ' + '-a'
+        if args['l']:
+            query += ' ' + '-l'
         output = get_output(args['q'])
         return { 'answer': output }, 200
